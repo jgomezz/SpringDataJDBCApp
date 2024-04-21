bb9d5c88d3761ea8e9a52b7496915c2dbbc4b056
jgomezz <jgomezz@gmail.com>
2024-04-21 13:13:43 -0500

    Update test

--- src/main/java/pe/edu/tecsup/springbootapp/repositories/CategoriaRepositoryImpl.java
+++ src/main/java/pe/edu/tecsup/springbootapp/repositories/CategoriaRepositoryImpl.java
@@ -2,9 +2,9 @@ package pe.edu.tecsup.springbootapp.repositories;
 
 import org.springframework.stereotype.Repository;
 import pe.edu.tecsup.springbootapp.entities.Categoria;
-import pe.edu.tecsup.springbootapp.services.CategoriaServiceImpl;
 
 import java.util.List;
+
 
 @Repository
 public class CategoriaRepositoryImpl implements CategoriaRepository {
