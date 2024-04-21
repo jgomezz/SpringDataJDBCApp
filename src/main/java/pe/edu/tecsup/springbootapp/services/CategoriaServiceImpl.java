fd73bcac542a403564397dd0e397e9dc6ec5ada4
jgomezz <jgomezz@gmail.com>
2024-04-21 12:23:15 -0500

    Update

---
+++ src/main/java/pe/edu/tecsup/springbootapp/services/CategoriaServiceImpl.java
@@ -0,0 +1,23 @@
+package pe.edu.tecsup.springbootapp.services;
+
+import org.springframework.stereotype.Service;
+import pe.edu.tecsup.springbootapp.entities.Categoria;
+import pe.edu.tecsup.springbootapp.repositories.CategoriaRepository;
+
+import java.util.List;
+
+@Service
+public class CategoriaServiceImpl implements CategoriaService{
+
+    CategoriaRepository categoriaRepository;
+
+    public CategoriaServiceImpl(CategoriaRepository categoriaRepository) {
+        this.categoriaRepository = categoriaRepository;
+    }
+
+    @Override
+    public List<Categoria> findAll() throws Exception {
+        return this.categoriaRepository .findAll();
+    }
+
+}
