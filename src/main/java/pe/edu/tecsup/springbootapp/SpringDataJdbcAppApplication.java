3fe0ba8bfc96d6ea5b57aead05f6d52c5168e119
jgomezz <jgomezz@gmail.com>
2024-04-21 11:38:15 -0500

    first commit

---
+++ src/main/java/pe/edu/tecsup/springbootapp/SpringDataJdbcAppApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.tecsup.springbootapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJdbcAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJdbcAppApplication.class, args);
+	}
+
+}
