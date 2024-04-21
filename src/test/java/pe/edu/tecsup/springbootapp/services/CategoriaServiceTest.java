2a7109b35aebb1b785f45bfa68e764da7d285bf4
jgomezz <jgomezz@gmail.com>
2024-04-21 12:31:37 -0500

    Create test

---
+++ src/test/java/pe/edu/tecsup/springbootapp/services/CategoriaServiceTest.java
@@ -0,0 +1,14 @@
+package pe.edu.tecsup.springbootapp.services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class CategoriaServiceTest {
+
+    @Test
+    void findAll() {
+    }
+}
