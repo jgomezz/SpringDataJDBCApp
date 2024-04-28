66af88bece75796510d1e1d5c3136f46b9eb5aaa
jgomezz <jgomezz@gmail.com>
2024-04-28 09:28:02 -0500

    Update ProductoRepositoryImpl.java

---
+++ src/main/java/pe/edu/tecsup/springbootapp/repositories/ProductoRepositoryImpl.java
@@ -0,0 +1,96 @@
+package pe.edu.tecsup.springbootapp.repositories;
+
+import lombok.extern.java.Log;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+import pe.edu.tecsup.springbootapp.entities.Categoria;
+import pe.edu.tecsup.springbootapp.entities.Producto;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+/**
+ *
+ */
+class ProductoRowMapper implements RowMapper<Producto> {
+
+	@Override
+	public Producto mapRow(ResultSet rs, int rowNum) throws SQLException {
+
+		// Set categoria
+		Categoria categoria = new Categoria();
+		categoria.setId(rs.getLong("categorias_id"));
+		categoria.setNombre(rs.getString("categorias_nombre"));
+
+		// Set producto
+		Producto producto = new Producto();
+		producto.setId(rs.getLong("id"));
+		producto.setCategorias_id(rs.getLong("categorias_id"));
+		producto.setCategoria(categoria);
+		producto.setNombre(rs.getString("nombre"));
+		producto.setDescripcion(rs.getString("descripcion"));
+		producto.setPrecio(rs.getDouble("precio"));
+		
+		if (rs.wasNull())
+			producto.setPrecio(null);
+
+		producto.setStock(rs.getInt("stock"));
+		producto.setImagen_nombre(rs.getString("imagen_nombre"));
+		producto.setImagen_tipo(rs.getString("imagen_tipo"));
+		producto.setImagen_tamanio(rs.getLong("imagen_tamanio"));
+		
+		if (rs.wasNull())
+			producto.setImagen_tamanio(null);
+
+		producto.setEstado(rs.getInt("estado"));
+		
+		return producto;
+	}
+	
+}
+
+/**
+ *
+ */
+@Log
+@Repository
+public class ProductoRepositoryImpl implements ProductoRepository {
+
+	JdbcTemplate jdbcTemplate;
+
+    public ProductoRepositoryImpl(JdbcTemplate jdbcTemplate) {
+    	this.jdbcTemplate = jdbcTemplate;
+	}
+
+	/**
+	 *
+	 * @return
+	 * @throws Exception
+	 */
+	@Override
+	public List<Producto> findAll() throws Exception {
+		log.info("call findAll()");
+		String sql =
+				"""
+					SELECT p.id, p.categorias_id, c.nombre AS categorias_nombre, p.nombre, 
+						   p.descripcion, p.precio, p.stock, p.imagen_nombre, p.imagen_tipo, 
+						   p.imagen_tamanio, p.creado, p.estado 
+					FROM productos p  
+					INNER JOIN categorias c ON c.id = p.categorias_id
+					WHERE estado=1 
+					ORDER BY id
+				""";
+		
+		List<Producto> productos = jdbcTemplate.query(sql, new ProductoRowMapper());
+
+		log.info("Productos: " + productos);
+
+		return productos;
+	}
+
+
+}
